9bf99817b6b2b95c90ee916bcc6812965a0aedbd
boconnor20 <boconnor20@stuy.edu>
2021-01-12 12:05:18 -0500

    initialized files

---
+++ Radix.java
@@ -0,0 +1,21 @@
+public class Radix{
+public static int nth(int n, int col){
+/*get nth digit of an int, where 0 is the ones column, 1 is the tens column etc.
+nth(123,1) -> 2
+nth(-123,1) -> 2
+nth(123,2) -> 1
+nth(-123,2) -> 1
+*/
+}
+
+public static int length(int n){
+/*return the number of digits in n.
+length(0) -> 1
+length(15) -> 2
+length(-10) -> 2
+length(5112) -> 4*/
+}
+public static void merge(MyLinkedList original,MyLinkedList[]buckets){
+
+}
+}
