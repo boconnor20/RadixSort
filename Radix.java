a693b24edc6e0b72bfecc9e4629eff333794c2bf
boconnor20 <boconnor20@stuy.edu>
2021-01-12 12:16:10 -0500

    created a working nth

--- Radix.java
+++ Radix.java
@@ -1,21 +1,27 @@
 public class Radix{
 public static int nth(int n, int col){
-/*get nth digit of an int, where 0 is the ones column, 1 is the tens column etc.
-nth(123,1) -> 2
-nth(-123,1) -> 2
-nth(123,2) -> 1
-nth(-123,2) -> 1
-*/
+  /*get nth digit of an int, where 0 is the ones column, 1 is the tens column etc.
+  nth(123,1) -> 2
+  nth(-123,1) -> 2
+  nth(123,2) -> 1
+  nth(-123,2) -> 1
+  */
+  int x=Math.abs(n/(int)(Math.pow(10,col)));
+  return x %10;
 }
 
-public static int length(int n){
+//public static int length(int n){
 /*return the number of digits in n.
 length(0) -> 1
 length(15) -> 2
 length(-10) -> 2
 length(5112) -> 4*/
-}
-public static void merge(MyLinkedList original,MyLinkedList[]buckets){
+//}
+//public static void merge(MyLinkedList original,MyLinkedList[]buckets){
 
+//}
+public static void main(String[] args) {
+  System.out.println(nth(123,1));
+System.out.println(  nth(-123,1));
 }
 }
